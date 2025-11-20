7452e95da2967febc970322dea2bb5369125accc
Cas Meulenbelt <c.meulenbelt@student.han.nl>
2025-11-20 12:15:53 +0100

    Elixir project geïnstalleerd.

---
+++ protocol/analytics_service_elixir/lib/analytics_service_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AnalyticsServiceElixir do
+  @moduledoc """
+  Documentation for `AnalyticsServiceElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AnalyticsServiceElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
