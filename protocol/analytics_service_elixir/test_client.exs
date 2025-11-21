808e177a34e407c9237cf423edd863542ef4a4eb
Cas Meulenbelt <c.meulenbelt@student.han.nl>
2025-11-21 13:49:59 +0100

    Added: Test bij AnalyticsService

---
+++ protocol/analytics_service_elixir/test_client.exs
@@ -0,0 +1,14 @@
+{:ok, socket} = :gen_tcp.connect({127, 0, 0, 1}, 7001, [:binary, packet: :raw, active: false])
+
+msg = %{
+  "id" => "1",
+  "action" => "leaderboard",
+  "resource" => "/leaderboard",
+  "payload" => %{}
+}
+
+line = Jason.encode!(msg) <> "\n"
+:gen_tcp.send(socket, line)
+
+{:ok, response} = :gen_tcp.recv(socket, 0)
+IO.puts("Response: #{response}")
