ae8130955532eb806f021a987b5821ca8b338450
Cas Meulenbelt <c.meulenbelt@student.han.nl>
2025-12-02 11:52:02 +0100

    refactor: Analytics toegevoegd aan service_registry.py

--- protocol/gateway/service_registry.py
+++ protocol/gateway/service_registry.py
@@ -3,5 +3,6 @@ import os
 SERVICES = {
     "/user":   (os.getenv("USER_HOST", "userservice"), int(os.getenv("USER_PORT", "6001"))),
     "/card":   (os.getenv("CARD_HOST", "cardcollection"), int(os.getenv("CARD_PORT", "4001"))),
-    "/battle": (os.getenv("BATTLE_HOST", "battleengine"), int(os.getenv("BATTLE_PORT", "6003")))
+    "/battle": (os.getenv("BATTLE_HOST", "battleengine"), int(os.getenv("BATTLE_PORT", "6003"))), 
+    "/analytics": (os.getenv("ANALYTICS_HOST", "analyticsservice"), int(os.getenv("ANALYTICS_POST", "7001")))
 }
